2ab9833258e709a8ec308dfd9b1ccbd7a7011df9
Stanislav Shvedov <79158732788@yandex.ru>
2025-03-03 18:11:55 +0300

    create app, maked based models.py

---
+++ backend/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        """Return a string representation of the Shop object using its name."""
+        return self.name
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        """Return a string representation of the ProductCategory object using its name."""
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    category_id = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return a string representation of the Product object using its name."""
+        return self.name
+
+
+class ShopProduct(models.Model):
+    shop_id = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False)
+
+
+class Parameters(models.Model):
+    screen_size = models.IntegerField(null=True)
+    resolution = models.IntegerField(null=True)
+    internal_memory = models.IntegerField(null=True)
+    color = models.CharField(max_length=50, null=True)
+    smart_tv = models.BooleanField(null=True)
+    capacity = models.IntegerField(null=True)
+
+
+class ProductInfo(models.Model):
+    model = models.CharField(max_length=100, null=False)
+    price = models.IntegerField(null=False)
+    price_rrc = models.IntegerField(null=False)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    parameters_id = models.ForeignKey(Parameters, on_delete=models.CASCADE)
+
+    def __str__(self):
+
+        return self.model
+
+
+class Order(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField(null=False)
+
+
+class OrderProduct(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False)
